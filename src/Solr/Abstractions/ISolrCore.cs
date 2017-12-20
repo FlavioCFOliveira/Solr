8435b82d2d294a4dcceba6b53b8036687dae4c15
FLAVIOCFO-LAPTO\o_mou <geral@flaviooliveira.com>
2017-12-20 23:22:01 +0000

    Base project structure definition

---
+++ src/Solr/Abstractions/ISolrCore.cs
@@ -0,0 +1,23 @@
+﻿namespace Solr.Abstractions
+{
+    using Solr.Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface ISolrCore<T>
+    {
+
+        string CoreName { get; }
+
+        Uri SolrUri { get; }
+
+        Task<SolrResponse> Add(T obj);
+
+        Task<SolrResponse> Delete(T obj);
+
+        Task<QueryResponse> Query(T obj);
+        
+    }
+}
---
+++ src/Solr/SolrCore.cs
@@ -0,0 +1,50 @@
+﻿namespace Solr
+{
+    using Solr.Abstractions;
+    using Solr.Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SolrCore<T> : ISolrCore<T>
+    {
+        readonly Uri baseUri;
+        readonly string coreName;
+        readonly HttpClient client;
+
+        public SolrCore(string baseAddress, string coreName)
+        {
+            this.baseUri = new Uri(baseAddress);
+            this.coreName = coreName;
+
+            this.client = new HttpClient()
+            {
+                BaseAddress = this.baseUri
+            };
+
+        }
+
+        public string CoreName { get { return this.coreName; } }
+
+        public Uri SolrUri { get { return this.baseUri; } }
+
+        public Task<SolrResponse> Add(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SolrResponse> Delete(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<QueryResponse> Query(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
